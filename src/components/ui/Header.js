d74855713d15ba4343998b876290a51717fcf4ce
George Maia <georgemaia@gmail.com>
2020-07-07 15:18:42 -0300

    pokstore logo

---
+++ src/components/ui/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../../img/pokestore.png'
+
+const Header = () => {
+    return (
+        <header>
+            <img src={logo} alt=''></img>
+        </header>
+    )
+}
+
+export default Header
