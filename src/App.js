cd1dba21801b1c61186679d9fb15eb56ec88795f
George Maia <georgemaia@gmail.com>
2020-07-07 22:22:50 -0300

    API conected

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios'
-import Header from './components/ui/Header'
+import { getAllPokemon } from './services/pokemon';
+import Header from './components/ui/Header';
 import './App.css';
 
 const App = () => {
-const [items, setItems] = useState([])
-const [isLoading, setIsLoading] = useState(true)
+  const [pokemonData, setPokemonData] = useState([]);
+  const [nestUrl, setNextUrl] = useState('');
+  const [prevUrl, setPrevUrl] = useState('');
+  const [loading, setLoading] = useState(true);
+  const initialUrl = 'https://pokeapi.co/api/v2/pokemon/';
 
-useEffect(() => {
-  const fetchItems = async () => {
-    const result = await axios(`https://pokeapi.co/api/v2/pokemon/`)
-
-    console.log(result.data)
-  }
-
-  fetchItems()
-}, [])
+  useEffect(() => {
+    async function fetchData() {
+      let response = await getAllPokemon(initialUrl);
+      console.log(response);
+      setNextUrl(response.next);
+      setPrevUrl(response.previous);
+      setLoading(false);
+    }
+    fetchData();
+  }, [])
 
   return (
     <div className="container">
       <Header />
+      { loading ? <h1>Loading...</h1> : (
+        <h1>Data is fetched</h1>
+      )}
     </div>
   );
 }
