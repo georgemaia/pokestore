6ed95dab9039fca9286780d65682069c6745b44a
George Maia <georgemaia@gmail.com>
2020-07-08 00:11:43 -0300

    Cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -7,31 +7,18 @@ function Card({ pokemon }) {
                 <img src={pokemon.sprites.front_default} alt="" />
             </div>
             <div className="Card__name">
-                {pokemon.name}
-            </div>
-            <div className="Card__types">
-                {
-                    pokemon.types.map(type => {
-                        return (
-                            <div className="Card__type">
-                                {type.type.name}
-                            </div>
-                        )
-                    })
-                }
+                <h2>{pokemon.name}</h2>
             </div>
             <div className="Card__info">
                 <div className="Card__data Card__data--weight">
-                    <p className="title">Weight</p>
-                    <p>{pokemon.weight}</p>
+                    <p className="title">Peso: {pokemon.weight} Libras</p>
+                    <p></p>
                 </div>
                 <div className="Card__data Card__data--weight">
-                    <p className="title">Height</p>
-                    <p>{pokemon.height}</p>
+                    <p className="title">Altura: {pokemon.height} Pés</p>
                 </div>
                 <div className="Card__data Card__data--ability">
-                    <p className="title">Ability</p>
-                    <p>{pokemon.abilities[0].ability.name}</p>
+                    <p className="title">Habilidade: {pokemon.abilities[0].ability.name}</p>
                 </div>
             </div>
         </div>
